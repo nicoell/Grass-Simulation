0433ca0700fb3c2a241fe8f44855630cd6078f17
Nico Ell <nicoell@uni-koblenz.de>
2017-11-29 14:07:50 +0100

    Changed terrainheightinput to use smoothed sampling
    Cannot handle inaccuracies between heightmaps, need to further investigate

--- Assets/Scripts/GrassSimulation/StandardInputs/TerrainHeightInput.cs
+++ Assets/Scripts/GrassSimulation/StandardInputs/TerrainHeightInput.cs
@@ -10,12 +10,19 @@ namespace GrassSimulation.StandardInputs
 
 		public override float GetHeight(float x, float y)
 		{
-			return _terrain.terrainData.GetInterpolatedHeight(x, y) / _terrain.terrainData.size.y;
+			int x0 = Mathf.FloorToInt(x * (_terrain.terrainData.heightmapWidth - 1)),
+				y0 = Mathf.FloorToInt(y * (_terrain.terrainData.heightmapHeight - 1));
+			float t0 = Mathf.SmoothStep(0f, 1f, x * (_terrain.terrainData.heightmapWidth - 1) - x0),
+				t1 = Mathf.SmoothStep(0f, 1f, y * (_terrain.terrainData.heightmapHeight - 1) - y0);
+			var h = _terrain.terrainData.GetHeights(x0, y0, 2, 2);
+			return Mathf.LerpUnclamped(Mathf.LerpUnclamped(h[0, 0], h[0, 1], t0), Mathf.LerpUnclamped(h[1, 0], h[1, 1], t0), t1);
+			//return _terrain.terrainData.GetInterpolatedHeight(x, y) / _terrain.terrainData.size.y;
 		}
 
 		public override Vector2 GetSamplingRate()
 		{
-			return new Vector2(1.0f / _terrain.terrainData.heightmapWidth, 1.0f / _terrain.terrainData.heightmapHeight);
+			return new Vector2(1.0f / (_terrain.terrainData.heightmapWidth - 1),
+				1.0f / (_terrain.terrainData.heightmapHeight - 1));
 		}
 	}
 }
