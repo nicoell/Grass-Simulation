92bd973114e332fe10b7c9e78b750012e9373505
Nico Ell <nicoell@uni-koblenz.de>
2018-03-06 03:02:23 +0100

    Fix RandomGrassMap Input

--- Assets/Scripts/GrassSimulation/StandardInputs/RandomGrassMapInput.cs
+++ Assets/Scripts/GrassSimulation/StandardInputs/RandomGrassMapInput.cs
@@ -7,18 +7,20 @@ namespace GrassSimulation.StandardInputs
 	public class RandomGrassMapInput : GrassMapInput, IInitializableWithCtx
 	{
 		private Random _random;
+		private SimulationContext _context;
 		private byte _typeCount;
 
 		public void Init(SimulationContext context)
 		{
+			_context = context;
 			_random = context.Random;
 			_typeCount = context.BladeContainer.GetTypeCount();
 		}
 
-		public override int GetGrassType(float x, float y, float z) { return (int) (_random.NextDouble() * _typeCount); }
+		public override int GetGrassType(float x, float y, float z) { return _context.BladeContainer.GetGrassTypeByDistribution((float) _random.NextDouble()); }
 
-		public override float GetDensity(float x, float y, float z) { return x * z; }
+		public override float GetDensity(float x, float y, float z) { return 1f; }
 
-		public override float GetHeightModifier(float x, float y, float z) { return x * z; }
+		public override float GetHeightModifier(float x, float y, float z) { return 1f; }
 	}
 }
